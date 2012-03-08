7997a3feeb986c2b30b955ac89cba2fcd02a16c3
Daniel Sommermann <dcsommer@gmail.com>
2012-03-08 13:16:56 -0800

    Added easy window switching using meta-arrowkey

--- .emacs.d/misc.el
+++ .emacs.d/misc.el
@@ -35,3 +35,18 @@
 
 ; keyboard scroll one line at a time
 (setq scroll-step 1)
+
+; easy window switching
+(defun select-next-window ()
+  "Switch to the next window" 
+  (interactive)
+  (select-window (next-window)))
+
+(defun select-previous-window ()
+  "Switch to the previous window" 
+  (interactive)
+  (select-window (previous-window)))
+
+(global-set-key (kbd "M-<right>") 'select-next-window)
+(global-set-key (kbd "M-<left>")  'select-previous-window)
+

ce690c52660bd6cff90e0d76cfeb790b27de6b98
Daniel Sommermann <dcsommer@gmail.com>
2012-06-28 15:35:30 -0700

    Added expand region but need to figure out issue with terminal mode emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,12 @@
 (add-to-list 'load-path (file-name-directory load-file-name))
-; my standard preferences
+(let ((default-directory (file-name-directory load-file-name)))
+     (normal-top-level-add-subdirs-to-load-path))
+; my standard preferences, look and feel, etc
 (load "misc")
 ; matlab major mode
 (load "matlab")
 (setq auto-mode-alist (cons '("\\.m$" . matlab-mode) auto-mode-alist))
+; expand region
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+; TODO figure out why above doesn't work in -nw mode
